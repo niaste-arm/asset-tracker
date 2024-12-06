52b8a57be3916a35b47f558b56fc0e8db051830f
Niall Stewart <niall.stewart@arm.com>
2024-12-06 17:48:54 +0000

    [FIX] Fix database error in tests

--- assettracker/settings.py
+++ assettracker/settings.py
@@ -82,18 +82,20 @@ WSGI_APPLICATION = 'assettracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    # For Render PostgreSQL
-    'default': dj_database_url.config(
-        conn_max_age=600
-    )
-    # OLD
-    #{
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-}
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        # For Render PostgreSQL
+        'default': dj_database_url.config(
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- webapp/tests.py
+++ webapp/tests.py
@@ -5,7 +5,7 @@ from webapp.forms import AssetForm, EmployeeForm
 class EmployeeTestCase(TestCase):
     def setUp(self):
         self.employee = Employee.objects.create(full_name="Adam Smith", 
-                                date_of_birth="1969, 7, 20", 
+                                date_of_birth="1969-7-20", 
                                 address="123 Street Avenue")
 
     def test_employee_returns_name(self):
@@ -19,7 +19,7 @@ class FormTestCase(TestCase):
     def test_add_asset_form(self):
         ## Create sample employee for test case asset owner
         employee = Employee.objects.create(full_name="Adam Smith", 
-                                date_of_birth="1969, 7, 20", 
+                                date_of_birth="1969-7-20", 
                                 address="123 Street Avenue")
         ## Create sample asset for testing
         asset = Asset.objects.create(asset_owner=employee, 
@@ -32,7 +32,7 @@ class FormTestCase(TestCase):
 
     def test_add_employee_form(self):
         employee = Employee.objects.create(full_name="Adam Smith", 
-                                date_of_birth="1969, 7, 20", 
+                                date_of_birth="1969-7-20", 
                                 address="123 Street Avenue")
         
         data = {'full_name' : employee.full_name, 'date_of_birth' : employee.date_of_birth, 'address' : employee.address}
